4f2be6c18a08762fee9f13471a721980b8d65635
rtalrejagammastack <rtalreja@grepruby.io>
2023-04-06 12:06:22 +0530

    Confirmable Mail Feature Added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,6 +76,16 @@ Rails.application.configure do
 
   # Action Mailer
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name:      Rails.application.credentials.development[:email][:mail_id],
+    password:       Rails.application.credentials.development[:email][:password],
+    address:       'smtp.gmail.com',
+    port:          '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   # MDBootstrap form issue resolve
   ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
